0798f6447c7222ba128318778dce6de761b6c736
Nicholas Babu <nijoba33@gmail.com>
2023-05-01 17:16:45 +0300

    updated doc comment

--- src/helpers/stripTocBlockContent.js
+++ src/helpers/stripTocBlockContent.js
@@ -1,10 +1,9 @@
-// Replace all content within the TOC block
 /**
- * Replaces punctuation characters from the given text string.
+ * Replace all content within the TOC block from the given string.
  *
- * @param {string} text The text to remove the punctuation characters for.
+ * @param {string} text The text to remove the TOC content for.
  *
- * @return {string} The sanitized text.
+ * @return {string} The text after TOC content is removed.
  */
 export default ( text ) => text
 	.replace( /<!-- wp:rank-math\/toc-block([\s\S]*?)<!-- \/wp:rank-math\/toc-block -->/g, '' )
